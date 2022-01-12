7c37e90a65b4e7bf6fa2b1efc87c1d2a92973f92
Alisha Bajaj <85244724+bajajalisha@users.noreply.github.com>
2022-01-11 20:14:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from ssl import ALERT_DESCRIPTION_HANDSHAKE_FAILURE
+#from ssl import ALERT_DESCRIPTION_HANDSHAKE_FAILURE
 from nltk.tokenize import word_tokenize
 import nltk
 nltk.download('punkt')
@@ -15,15 +15,15 @@ def make_haiku(first,second,third):
 
 def make_line(vowels_allowed,word_list):
     counter = []
-    
+		
     for word in word_list:
         if words.exception_checker(word):
             counter.append(words.exception_vowels(word))
         else:
             counter.append(vowel_counter(word))
-    
+
     if sum(counter) == vowels_allowed:
-        return ' '.join(word_list)
+      return ' '.join(word_list)
     
     elif sum(counter) < vowels_allowed:
         vowels_left = vowels_allowed - sum(counter)
@@ -42,33 +42,38 @@ def vowel_counter(word):
     for i in word:
         if i in vowels:
             counter = counter + 1
+		
+    return counter
      
+def hack_haiku():
+		first_sentence = input("Enter first sentence:") 
+		second_sentence = input("Enter second sentence:")
+		third_sentence = input("Enter third sentence:")  
+		print('\n')
 
-#first_sentence = input("Enter first sentence:") 
-#second_sentence = input("Enter second sentence:")
-#third_sentence = input("Enter third sentence:")
+		first_list = word_tokenize(first_sentence)
+		second_list = word_tokenize(second_sentence)
+		third_list = word_tokenize(third_sentence)
 
-first_sentence = "hello I am alisha"
-second_sentence = "roses only fall when in need"
-third_sentence = "I like to eat cake"     
+		make_haiku(first_list,second_list,third_list)
 
-first_list = word_tokenize(first_sentence)
-second_list = word_tokenize(second_sentence)
-third_list = word_tokenize(third_sentence)
+		print('\n')
+		exception = input('Is this haiku correct based on the number of syllables/Do the number of vowel letters match the number of syllables?(Y/N)')
 
-make_haiku(first_list,second_list,third_list)
-
-exception = input('Is this a haiku based on the vowel sounds/Do the number of vowel letters match the number of vowel sounds?(Y/N)')
-
-if exception.lower() == 'n':
-    print('Please include a word that you used where the number of vowel letters does not match the number of vowels sounds when prompted.')
-    word = input('Enter the word:')
-    vowel_sounds = input('Enter the number of vowel sounds:')
+		if exception.lower() == 'n':
+				print('\nPlease include a word that you used where the number of vowel letters does not match the number of syllables (ex. cheese has 3 vowels but 1 syllable) when prompted.\n')
+				word = input('Enter the word:')
+				vowel_sounds = int(input('Enter the number of vowel sounds:'))
     
-    words.exception_adder(vowel_sounds,word)
+				words.exception_adder(vowel_sounds, word)
     
-    print('Thanks for helping make this better!')    
-else:
-    print('Good to hear!')
+				print('\nThanks for helping make HackHaiku better!')    
+		else:
+				print('\nGood to hear!')
 
-print('Refresh to try a new haiku!') 
+		print('Time to try a new haiku!\n')
+
+		hack_haiku()
+
+print('Welcome to HackHaiku\n')
+hack_haiku()
